3e66473f2aaecead1fa1771ed24b0d5c66a6e45f
AslamLert <gloommyday@gmail.com>
2021-09-01 23:16:39 +0700

    employees works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+
+const appRoutes: Routes = [
+  {path: '', component: EmployeesComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { EmployeesComponent } from './components/employees/employees.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
