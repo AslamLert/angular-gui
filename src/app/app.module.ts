9a2582890c51e5b7ed79064c4b9ab9c74d4c0d54
AslamLert <gloommyday@gmail.com>
2021-09-02 21:32:21 +0700

    form submit console

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -22,6 +23,7 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -23,4 +23,8 @@ export class EmployeesComponent implements OnInit {
     });
   }
 
+  insertData(){
+    console.log('Hello')
+  }
+
 }
